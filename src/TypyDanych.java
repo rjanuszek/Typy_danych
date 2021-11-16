11dbdf6ce401cfa80015913802a0415259faab4e
rjanuszek <rafaela.januszek@gmail.com>
2021-11-16 17:03:43 +0100

    Added Typy zmiennych

---
+++ src/TypyDanych.java
@@ -0,0 +1,25 @@
+public class TypyDanych {
+
+    public static void main(String[] args) {
+        // liczby całkowite
+        System.out.println(10);
+
+        // liczby zmienno przecinkowe
+        System.out.println(10.5);
+
+        // tekst
+        System.out.println("Tekst w Java");
+
+        // znak
+        System.out.println('a');
+
+        // prawda
+        System.out.println(true);
+
+        // fałsz
+        System.out.println(false);
+
+        // comentowanie lini
+//        System.out.println("Nic ważnego");
+    }
+}
