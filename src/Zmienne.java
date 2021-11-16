77b3eef2f2d66a375399021d075bd2d3ed46ca45
rjanuszek <rafaela.januszek@gmail.com>
2021-11-16 17:28:22 +0100

    Added Zmienne

---
+++ src/Zmienne.java
@@ -0,0 +1,31 @@
+public class Zmienne {
+
+    public static void main(String[] args)
+    {
+        // liczby całkowite
+//        int number10; // deklaracja zmiennej
+//        number10 = 10; // inicjalizacja zmiennej
+        int number = 10; // deklaracja + inicjalizacja zmiennej w jednym
+        System.out.println(number);
+
+        // liczby zmienno przecinkowe
+        double doubleNumber = 10.5;
+        System.out.println(doubleNumber);
+
+        // tekst
+        String someText = "text w Java";
+        System.out.println("someText");
+
+        // znak
+        char someChar = 's';
+        System.out.println(someChar);
+
+        // prawda
+        boolean trueValue = true;
+        System.out.println(trueValue);
+
+        // fałsz
+        boolean falseValue = false;
+        System.out.println(falseValue);
+    }
+}
