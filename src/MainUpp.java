ff283ac234520daf11a8a9121df8c9cee2d5dac6
rjanuszek <rafaela.januszek@gmail.com>
2021-11-18 17:41:00 +0100

    Added Zmienne

--- src/MainUpp.java
+++ src/MainUpp.java
@@ -1,8 +1,33 @@
-public class MainUpp
-{
-    public static void main(String[] args)
-    {
-        //Start aplikacji
-        System.out.println("Hello word");
+public class MainUpp {
+    public static void main(String[] args) {
+//        //Start aplikacji
+//        System.out.println("Hello word");
+
+        // Nazwa klas->  Piszemy CamelCase:
+        //    User, UserName
+
+        // zmienne->  Piszemy jak camelCase:
+        // String superUser
+
+        //int number = 10;
+        // boolean isTrue = true;
+
+        // formatownie kodu
+
+//        if (10 > 0) {
+//            System.out.println("true");
+//        } else {
+//            System.out.println("false");
+//        }
+
+//        System.out.println(10 % 5);
+//        System.out.println(10 % 4);
+//        System.out.println(10 % 11);
+        if (10 % 3 == 0) {
+            System.out.println("Liczba parzysta");
+        } else {
+            System.out.println("Liczba nieparzytsa");
+        }
     }
 }
+
---
+++ src/Operatory.java
@@ -0,0 +1,59 @@
+public class Operatory {
+    public static void main(String[] args)
+    {
+////        -> operatory matematyczne
+//        System.out.println(10 + 10);
+//        System.out.println(10 - 10);
+//        System.out.println(10 * 10);
+//        System.out.println(10 / 10);
+//
+//        double a = 10;
+//        double b = 4;
+//        System.out.println(a / b);
+//
+////       -> operatory porównia
+//        int numOne = 10;
+//        int numTwo = 2;
+//
+//        System.out.println(numOne > numTwo);
+//        System.out.println(numOne < numTwo);
+//        System.out.println(numOne == numTwo);
+//        System.out.println(numOne != numTwo);
+//
+////        -> operatory logiczne lub/ i
+////        || lub
+//        System.out.println(numOne > numTwo || numOne == 1); // fałsz || prawda = prawda
+//        System.out.println(numOne == numTwo || numOne != 1); // fałsz || fałsz = fałsz
+//        System.out.println(numOne < numTwo || numOne != 1); // prawda || brak sprawdzenia = prawda
+////        && i
+//        System.out.println(numOne < numTwo && numOne == 1); // prawda && prawda = prawda
+//        System.out.println(numOne > numTwo && numOne == 1); // fałsz && prawda = fałsz
+//        System.out.println(numOne > numTwo && numOne != 1); // fałsz && fałsz = fałsz
+
+//        if(numOne < numTwo){
+//            System.out.println("number one is less than number two");
+//        } else {
+//            System.out.println("number one is greater than number two");
+//        }
+//        String homePage = "Home";
+//        String loginPage = "Login";
+//        String contactPage = "Contact";
+//
+//        String page= "jakaś strona";
+//        switch (page){
+//            case "Home":
+//                System.out.println("Przełączam się do strony Home");
+//                break;
+//            case "Login":
+//                System.out.println("Przełączam się do strony Login");
+//                break;
+//            case "Contact":
+//                System.out.println("Przełączam się do strony Contact");
+//                break;
+//            default:
+//            System.out.println("Nie znam takiej strony");
+//        }
+
+
+    }
+}
