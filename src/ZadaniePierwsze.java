24593588413d8ad210e981294660b51a0a15d04a
rjanuszek <rafaela.januszek@gmail.com>
2022-01-17 18:44:25 +0100

    Added PracaDomowaZadaniePierwsze

---
+++ src/ZadaniePierwsze.java
@@ -0,0 +1,32 @@
+public class ZadaniePierwsze {
+    public static void main(String[] args) {
+// 1. 4 zmienne int
+// 2. Sprawdzić czy suma dwóch pierwszych jest parzysta
+// 3. Sprawdzić czy suma dwóch pierwszych jest parzysta
+// 4. Jeżeli dwie sumy są parzyste to wyświetl informację, że wszystkie liczby są parzyste
+
+        int numberOne = 2;
+        int numberTwo = 2;
+        int numberThree = 4;
+        int numberFour = 4;
+
+
+        if ((numberOne + numberTwo) % 2 == 0) {
+            System.out.println("Jest to liczba parzysta");
+        } else {
+            System.out.println("Nie jest to liczba parzytsa");
+        }
+
+        if ((numberThree + numberFour) % 2 == 0) {
+            System.out.println("Jest to liczba parzysta");
+        } else {
+            System.out.println("Nie jest to liczba parzytsa");
+        }
+
+        if ((numberOne + numberTwo) % 2 == 0 && (numberThree + numberFour) % 2 == 0) {
+            System.out.println("Wszystkie liczby są parzyste");
+        } else {
+            System.out.println("Nie wszytskie liczby są parzyste");
+        }
+    }
+}
